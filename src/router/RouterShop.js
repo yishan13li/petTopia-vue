08b8201b0f7fecbecc79d21a83b0165cce2191b9
ispan yon <pzbks22182@gmail.com>
2025-03-12 16:24:58 +0800

    商品列表、商品詳情 都轉為vue

--- src/router/RouterShop.js
+++ src/router/RouterShop.js
@@ -2,6 +2,8 @@ import ShopIndex from '@/views/shop/ShopIndex.vue';
 import ShopCheckout from '@/views/shop/ShopCheckout.vue';
 import ShopOrderHistory from '@/views/shop/ShopOrderHistory.vue';
 import ShopOrderDetail from '@/views/shop/ShopOrderDetail.vue';
+import ShopProducts from '@/views/shop/ShopProducts.vue';
+import ShopProductDetail from '@/views/shop/ShopProductDetail.vue';
 
 export default [
     {
@@ -25,6 +27,18 @@ export default [
         path: '/shop/orders/:orderId',
         name: 'ShopOrderDetail',
         component: ShopOrderDetail
-    }
-  ];
-  
+    },
+
+    {
+        path: '/shop/products',
+        name: 'ShopProducts',
+        component: ShopProducts
+    },
+
+    {
+        path: '/shop/productDetail',
+        name: 'ShopProductDetail',
+        component: ShopProductDetail
+    },
+
+];
--- src/views/shop/ShopProductDetail.vue
+++ src/views/shop/ShopProductDetail.vue
@@ -38,7 +38,8 @@
 
                         <div v-for="(size, index) in sizeList" class="form-check">
                             <input class="size-radio form-check-input" type="radio" name="size" :id="'size' + index"
-                                :value="size.id" v-model="selectedSizeRadio" @click="onClickRadio(size)">
+                                :value="size.id" v-model="selectedSizeRadio" ref="sizeRadios"
+                                @change="onChangeRadio(size, index)" @click="onClickRadio(size, index)">
                             <label class="form-check-label no-select" :for="'size' + index">
                                 {{ size.name }}
                             </label>
@@ -57,7 +58,8 @@
 
                         <div v-for="(color, index) in colorList" class="form-check">
                             <input class="color-radio form-check-input" type="radio" name="color" :id="'color' + index"
-                                :value="color.id">
+                                :value="color.id" v-model="selectedColorRadio" ref="colorRadios"
+                                @change="onChangeRadio(color, index)" @click="onClickRadio(color, index)">
                             <label class="form-check-label no-select" :for="'color' + index">
                                 {{ color.name }}
                             </label>
@@ -71,17 +73,18 @@
                 <!-- 購買數量 -->
                 <div class="d-flex align-items-center mb-3">
                     <label class="form-label">數量</label>
-                    <button class="btn btn-outline-secondary ms-4" id="decrease-btn" :disabled="isDisabled"
+                    <button class="btn btn-outline-secondary ms-4" id="decrease-btn" :disabled="isQuantityInputDisabled"
                         @click="onClickDecreaseBtn">-</button>
                     <input type="number" class="form-control text-center mx-2 no-spinner" id="product-quantity-input"
-                        v-model="quantity" :disabled="isDisabled" style="width: 60px;">
-                    <button class="btn btn-outline-secondary" id="increase-btn" :disabled="isDisabled"
+                        v-model="quantity" :disabled="isQuantityInputDisabled" @input="onQuantityInput"
+                        style="width: 60px;">
+                    <button class="btn btn-outline-secondary" id="increase-btn" :disabled="isQuantityInputDisabled"
                         @click="onClickIncreaseBtn">+</button>
-                    <label class="form-label ms-3" id="show-stock-quantity">還剩 {{ totalStockQuantity }} 件</label>
-                    <label class="form-label ms-3" id="messages"></label>
+                    <label class="form-label ms-3" id="show-stock-quantity">還剩 {{ showQuantity }} 件</label>
+                    <label class="form-label ms-3" id="messages" style="color: crimson;">{{ messages }}</label>
                 </div>
 
-                <button class="btn btn-primary w-100" id="add-to-cart-btn">加入購物車</button>
+                <button class="btn btn-primary w-100" id="add-to-cart-btn" @click="onClickAddToCartBtn">加入購物車</button>
 
             </div>
         </div>
@@ -89,13 +92,15 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch, watchEffect, computed, nextTick } from 'vue';
 import { useRoute } from 'vue-router';
 import axios from 'axios';
 
 const route = useRoute();
 
+// localhost 8080 path
 const path = `${import.meta.env.VITE_API_URL}`;
+// 商品列表的productDetailId
 const productDetailId = route.query.productDetailId;
 
 // 初始載入
@@ -105,7 +110,17 @@ const colorList = ref([]);
 const minPriceProduct = ref({});
 const maxPriceProduct = ref({});
 const productDetail = ref({});
-const totalStockQuantity = ref(0);
+const totalStockQuantity = ref(0);      // 商品實際庫存
+const productQuantityInCart = ref(0);   // 會員購物車內該商品的數量
+
+// 計算顯示庫存 = 商品實際庫存 - 會員購物車內該商品的數量
+const showQuantity = computed(() => {
+    return Number(totalStockQuantity.value) - Number(productQuantityInCart.value);
+});
+
+// 規格選項元件
+const sizeRadios = ref([]);
+const colorRadios = ref([]);
 
 // 已選擇的規格選項
 const selectedSizeRadio = ref(null);
@@ -114,13 +129,15 @@ const selectedColorRadio = ref(null);
 // 購買數量
 const quantity = ref(1);
 
-// 元件 +-按鍵和輸入數量 disabled
-const isDisabled = ref(false);
+// +-按鍵和輸入數量元件的disabled
+const isQuantityInputDisabled = ref(false);
+// 是否規格選項都選取
+const isAllOptionChecked = ref(false);
 
+// 提示訊息
+const messages = ref("");
 
-
-// 檢查所有選項是否已選擇
-let isAllOptionChecked = false;
+// #region 初始化 & 監聽 =================================================
 
 onMounted(async () => {
     getProductDetail();
@@ -133,7 +150,7 @@ async function getProductDetail() {
         url: `${path}/shop/productDetail?productDetailId=${productDetailId}`,
     })
         .then(response => {
-            // console.log(response.data);
+            console.log(response.data);
             productList.value = response.data.productList;
             sizeList.value = response.data.sizeList;
             colorList.value = response.data.colorList;
@@ -142,28 +159,127 @@ async function getProductDetail() {
             productDetail.value = response.data.productDetail;
             totalStockQuantity.value = response.data.totalStockQuantity;
 
-        })
 
+        })
         .catch(error => console.log(error));
+
 }
+
+watchEffect(() => {
+    console.log("選中的尺寸:", selectedSizeRadio.value);
+    console.log("選中的顏色:", selectedColorRadio.value);
+
+});
+
+watch([sizeList, colorList, selectedSizeRadio, selectedColorRadio], ([sizeList, colorList, selectedSizeRadioValue, selectedColorRadioValue]) => {
+    // 監聽 是否規格選項都選擇
+    if (sizeList.length === 0 && colorList.length === 0) {
+        setIsAllOptionChecked(true);
+    }
+    else {
+        if ((colorList.length === 0 && selectedSizeRadioValue !== null) ||
+            (sizeList.length === 0 && selectedColorRadioValue !== null) ||
+            (selectedSizeRadioValue !== null && selectedColorRadioValue !== null)) {
+            setIsAllOptionChecked(true);
+        }
+        else
+            setIsAllOptionChecked(false);
+
+    }
+
+
+});
+
+// #endregion ===============================================================
+
 
 // #region Event function =================================================
 
-// 規格選項 點擊事件
-function onClickRadio(currentRadio) {
-    console.log(currentRadio.id);
-    console.log(currentRadio.name);
-    console.log(selectedSizeRadio.value);
+// 規格選項 變更事件 (點選同一個選項不會觸發)
+async function onChangeRadio(currentRadio, index) {
+    // console.log(currentRadio.id);
+    // console.log(currentRadio.name);
+    // console.log("選擇的 size radio:", selectedSizeRadio.value);
+    // console.log("選擇的 color radio:", selectedColorRadio.value);
+
+    let optionName = event.target.name;
+
+    if ((selectedSizeRadio.value !== null && selectedColorRadio.value !== null) ||
+        (selectedSizeRadio.value !== null && colorList.value.length === 0) ||
+        (selectedColorRadio.value !== null && sizeList.value.length === 0)) {
+        confirmProduct();
+    }
+    else {
+        selectOneOption(currentRadio.id, optionName);
+    }
+
+
+
+}
+
+// 規格選項 點擊事件 => 取消選項選擇的狀態
+function onClickRadio(currentRadio, index) {
+    if (event.target.name === 'size') {
+        if (sizeRadios.value[index].checked && selectedSizeRadio.value === currentRadio.id) {
+            sizeRadios.value[index].checked = false;
+            selectedSizeRadio.value = null;
+
+            // 所有選項都被取消 or 一個選項被取消
+            if (selectedSizeRadio.value === null && selectedColorRadio.value === null) {
+                cancelAllOption();
+            }
+            else {
+                selectOneOption(selectedColorRadio.value, 'color');
+            }
+        }
+    }
+    else if (event.target.name === 'color') {
+        if (colorRadios.value[index].checked && selectedColorRadio.value === currentRadio.id) {
+            colorRadios.value[index].checked = false;
+            selectedColorRadio.value = null;
+
+            // 所有選項都被取消 or 一個選項被取消
+            if (selectedSizeRadio.value === null && selectedColorRadio.value === null) {
+                cancelAllOption();
+            }
+            else {
+                selectOneOption(selectedSizeRadio.value, 'size');
+            }
+        }
+    }
+
+
 }
 
 // +按鍵
 function onClickIncreaseBtn() {
-    quantity.value++;
+    if (Number(quantity.value) < Number(totalStockQuantity.value))
+        quantity.value++;
 }
 
 // -按鍵
 function onClickDecreaseBtn() {
-    quantity.value--;
+    if (Number(quantity.value) > 1)
+        quantity.value--;
+}
+
+function onQuantityInput() {
+    if (Number(quantity.value) < 1) {
+        quantity.value = 1;
+    }
+    else if (Number(quantity.value) > Number(totalStockQuantity.value)) {
+        quantity.value = Number(totalStockQuantity.value);
+    }
+}
+
+// 加入購物車
+function onClickAddToCartBtn() {
+    if (isAllOptionChecked.value) {
+        addToCart();
+    }
+    else {
+        messages.value = "請選擇商品規格";
+    }
 }
 
 // #endregion ===============================================================
@@ -172,9 +288,143 @@ function onClickDecreaseBtn() {
 
 // #region General function =================================================
 
+// FIXME: memberId改為登入後獲取
+// 當所有規格都被選擇時，確認選定商品
+async function confirmProduct() {
+    await axios({
+        method: 'post',
+        url: `${path}/shop/productDetail/api/getConfirmProductByDetailIdSizeIdColorId`,
+        params: {
+            memberId: 11,
+            productDetailId: productDetailId,
+            productSizeId: selectedSizeRadio.value !== null ? selectedSizeRadio.value : null,
+            productColorId: selectedColorRadio.value !== null ? selectedColorRadio.value : null,
+        }
+    })
+        .then(response => {
+            console.log(response.data);
+            productList.value = [response.data.product];
+            totalStockQuantity.value = response.data.product.stockQuantity;
+            productQuantityInCart.value = Number(response.data.productQuantityInCart) || 0;
+
+            // 除了被選擇的選項都設為 disabled = true
+            sizeRadios.value.forEach(sizeRadio => {
+                if (Number(sizeRadio.value) !== Number(selectedSizeRadio.value))
+                    sizeRadio.disabled = true;
+                else
+                    sizeRadio.disabled = false;
+            });
+            colorRadios.value.forEach(colorRadio => {
+                if (Number(colorRadio.value) !== Number(selectedColorRadio.value))
+                    colorRadio.disabled = true;
+                else
+                    colorRadio.disabled = false;
+            });
+
+        })
+        .catch(error => console.log(error));
+}
+
+// 當有單一規格選項被選擇時，過濾是否有符合該規格的顏色或尺寸，並切換為可點擊或不可點擊
+async function selectOneOption(optionId, optionName) {
+    await axios({
+        method: 'post',
+        url: `${path}/shop/productDetail/api/getProductByOption`,
+        params: {
+            productDetailId: productDetailId,
+            optionId: optionId,
+            optionName: optionName,
+        }
+    })
+        .then(response => {
+            // console.log(response.data);
+            productList.value = response.data.productList;
+            minPriceProduct.value = response.data.minPriceProduct;
+            maxPriceProduct.value = response.data.maxPriceProduct;
+            totalStockQuantity.value = response.data.totalStockQuantity;
+            productQuantityInCart.value = 0;
+
+            // 選擇尺寸 篩選顏色
+            if (optionName === 'size') {
+                colorRadios.value.forEach(colorRadio => {
+                    colorRadio.disabled = true;
+                    for (let i = 0; i < productList.value.length; i++) {
+                        if (Number(colorRadio.value) === Number(productList.value[i].productColor.id)) {
+                            colorRadio.disabled = false;
+                            break;
+                        }
+                    }
+                });
+            }
+            // 選擇顏色 篩選尺寸
+            else if (optionName === 'color') {
+                sizeRadios.value.forEach(sizeRadio => {
+                    sizeRadio.disabled = true;
+                    for (let i = 0; i < productList.value.length; i++) {
+                        if (Number(sizeRadio.value) === Number(productList.value[i].productSize.id)) {
+                            sizeRadio.disabled = false;
+                            break;
+                        }
+                    }
+                });
+            }
+
+        })
+        .catch(error => console.log(error));
+}
+
+// 取消所有規格選項
+async function cancelAllOption() {
+    await axios({
+        method: 'get',
+        url: `${path}/shop/productDetail/api/getProductByProductDetailId?productDetailId=${productDetailId}`,
+    })
+        .then(response => {
+            // console.log(response.data);
+            productList.value = response.data.productList;
+            minPriceProduct.value = response.data.minPriceProduct;
+            maxPriceProduct.value = response.data.maxPriceProduct;
+            totalStockQuantity.value = response.data.totalStockQuantity;
+            productQuantityInCart.value = 0;
+
+            // 重置全部radio disabled
+            sizeRadios.value.forEach(sizeRadio => {
+                sizeRadio.disabled = false;
+            });
+            colorRadios.value.forEach(colorRadio => {
+                colorRadio.disabled = false;
+            });
+
+        })
+        .catch(error => console.log(error));
+}
+
+// FIXME: memberId改為登入後獲取
+// 加入購物車
+async function addToCart() {
+    await axios({
+        method: 'post',
+        url: `${path}/shop/productDetail/api/addProductToCart`,
+        params: {
+            memberId: 11,
+            productDetailId: productDetailId,
+            productSizeId: selectedSizeRadio.value !== null ? selectedSizeRadio.value : null,
+            productColorId: selectedColorRadio.value !== null ? selectedColorRadio.value : null,
+            quantity: quantity.value,
+        }
+    })
+        .then(response => {
+            console.log(response.data);
+            messages.value = "成功加入購物車!"
+        })
+        .catch(error => console.log(error));
+
+}
+
 // 設定 +- 按鈕 和 輸入數量的disabled
-function setQuantityBtnAndInputDisabled(setDisabled) {
-    isDisabled = setDisabled;
+function setIsAllOptionChecked(setBool) {
+    isAllOptionChecked.value = setBool;
+    isQuantityInputDisabled.value = !setBool;
 }
 
 // #endregion ===============================================================

76d7990fd13532c8635f1f59b4c24a2f31a264ca
Melody <melodyshan0504@gmail.com>
2025-03-20 10:02:00 +0800

    前台port固定5173，後台應為5174

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+
   server: {
     host: '0.0.0.0',
+    port: 5173, 
     allowedHosts: ['2619-59-125-142-166.ngrok-free.app'],
   },
   plugins: [

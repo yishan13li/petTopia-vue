821f97ec6ce9dd5d44882a45d663d997bf812eac
danny841114 <danny841114@gmail.com>
2025-03-23 11:53:18 +0800

    活動詳情頁更新

--- src/views/activity/ActivityDetail.vue
+++ src/views/activity/ActivityDetail.vue
@@ -7,12 +7,38 @@
     >
       <div class="row align-items-center mt-xl-5">
         <div class="h-auto offset-md-1 col-md-5">
-          <!-- <img
-            :src="vendor.logoImgBase64"
-            alt="店家圖片"
-            class="img-fluid rounded-4"
-            width="400"
-          /> -->
+          <div class="d-flex justify-content-center">
+            <button class="swiper-button-prev custom-prev"></button>
+            <Swiper
+              :modules="[Pagination, Navigation]"
+              :pagination="{
+                clickable: true,
+              }"
+              :navigation="{
+                nextEl: '.custom-next',
+                prevEl: '.custom-prev',
+              }"
+            >
+              <SwiperSlide
+                v-for="(image, index) in activityImageList"
+                :key="index"
+                ><div
+                  style="
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                  "
+                >
+                  <img
+                    :src="image.imageBase64"
+                    class="img-fluid rounded-4"
+                    alt="image"
+                    style="max-width: 500px; max-height: 300px; margin: 10px"
+                  />
+                </div> </SwiperSlide
+            ></Swiper>
+            <button class="swiper-button-next custom-next"></button>
+          </div>
         </div>
 
         <div class="col-md-5 mt-5 mt-md-0">
@@ -51,9 +77,9 @@
             ><button
               style="margin-left: 10px"
               class="btn btn-primary btn-lg text-uppercase fs-5 rounded-4 me-4"
-              @click="registActivity()"
+              @click="registActivityConfirm()"
             >
-              報名
+              {{ registractionStatus }}
             </button>
             <button
               class="btn btn-primary btn-lg text-uppercase fs-5 rounded-4 me-4"
@@ -73,13 +99,16 @@
             <div class="d-flex flex-wrap mt-3">
               <button
                 class="btn btn-primary btn-lg text-uppercase fs-5 rounded-4 me-4"
+                @click="toggleLike()"
               >
-                收藏
+                {{ likeStatus }}
               </button>
               <button
                 class="btn btn-primary btn-lg text-uppercase fs-5 rounded-4 me-4"
+                :disabled="isAddReviewDisabled"
+                @click="openComment()"
               >
-                留言
+                {{ addReviewButton }}
               </button>
               <button
                 class="btn btn-primary btn-lg text-uppercase fs-5 rounded-4 me-4"
@@ -95,37 +124,6 @@
   </div>
   <!-- 主要內容結束 -->
 
-  <!-- 圖片區開始 -->
-  <div
-    class="padding-medium mt-xl-5"
-    style="padding: 20px"
-    v-if="activityImageList.length != 0"
-  >
-    <div class="container">
-      <div
-        class="section-header d-md-flex justify-content-between align-items-center mb-3"
-      >
-        <h2 class="display-6 fw-normal">活動圖片列表</h2>
-      </div>
-    </div>
-
-    <div
-      class="container rounded-4"
-      style="background-color: #f9f3ec; padding: 20px"
-    >
-      <!-- 這裡還要補寫放大圖片功能 -->
-      <img
-        v-for="(image, index) in activityImageList"
-        :key="index"
-        :src="image.imageBase64"
-        class="img-fluid rounded-4"
-        alt="image"
-        style="max-width: 500px; max-height: 300px; margin: 10px"
-      />
-    </div>
-  </div>
-  <!-- 圖片區結束 -->
-
   <!-- 留言區開始 -->
   <div class="container" style="padding: 20px" v-if="reviewList.length != 0">
     <div
@@ -135,7 +133,7 @@
     </div>
 
     <div v-for="review in reviewList" :key="review.reviewId">
-      <div class="comment" :data-id="review.reviewId">
+      <div class="comment">
         <div
           class="container rounded-3"
           style="background-color: #f9f3ec; padding: 20px"
@@ -143,13 +141,12 @@
           <div class="row">
             <div class="col-lg-3">
               <div class="image-container">
-                <!-- <img
+                <img
                   class="img-fluid rounded-4"
                   :src="review.profilePhotoBase64"
                   alt="alternative"
                   style="max-width: 200px; max-height: 200px"
-                /> -->
-                <!-- 放user照片  -->
+                />
               </div>
             </div>
 
@@ -157,7 +154,6 @@
               <div class="text-container" style="padding-top: 20px">
                 <h2>
                   <b>{{ review.name }}</b>
-                  <!-- 放user名字  -->
                 </h2>
 
                 <p>發表時間：{{ review.reviewTime }}</p>
@@ -173,12 +169,14 @@
                 <div class="d-flex flex-wrap mt-3">
                   <button
                     class="btn btn-outline-dark btn-lg text-uppercase fs-5 rounded-4 me-4"
+                    @click="openRewirte(review.reviewId)"
                   >
                     修改
                   </button>
 
                   <button
                     class="btn btn-outline-dark btn-lg text-uppercase fs-5 rounded-4 me-4"
+                    @click="deleteComment(review.reviewId)"
                   >
                     刪除
                   </button>
@@ -286,10 +284,62 @@
     </div>
   </div>
   <!-- 報名狀態視窗 -->
+
+  <!-- 留言視窗 -->
+  <div v-if="isPopupCommentVisible" class="overlay">
+    <div class="popup">
+      <h3><b>新增留言</b></h3>
+      <textarea
+        placeholder="輸入感想"
+        rows="5"
+        col="10"
+        style="resize: none"
+        v-model="commentForm.content"
+        required
+      ></textarea>
+      <div>
+        <button
+          class="btn btn-outline-dark btn-1g text-uppercase fs-5 rounded-4"
+          style="margin: 5px"
+          @click="closeComment()"
+        >
+          關閉
+        </button>
+        <button
+          v-if="commentButton"
+          class="btn btn-outline-dark btn-1g text-uppercase fs-5 rounded-4"
+          style="margin: 5px"
+          @click="submitComment()"
+        >
+          送出
+        </button>
+        <button
+          v-if="rewriteButton"
+          class="btn btn-outline-dark btn-1g text-uppercase fs-5 rounded-4"
+          style="margin: 5px"
+          @click="submitRewrite(reviewIdForRewrite)"
+        >
+          修改
+        </button>
+      </div>
+    </div>
+  </div>
+  <!-- 留言視窗 -->
 </template>
 
 <script setup>
 import { ref, onMounted, watch } from "vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination } from "swiper/modules";
+import { Navigation } from "swiper/modules";
+import Swal from "sweetalert2";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+/* 0. 取得member */
+let memberId = ref(16); // 之後要改
+let member = ref({ memberId: 16 }); // 之後要改
 
 /* 1. activityId及預設游標 */
 const props = defineProps({
@@ -315,7 +365,7 @@ const fetchActivityData = async () => {
 onMounted(fetchActivityData);
 
 /* 3. 活動圖片列表 */
-const activityImageList = ref([]);
+const activityImageList = ref({ id: "", imageBase64: "" });
 
 const fetchActivityImageList = async () => {
   try {
@@ -334,6 +384,16 @@ onMounted(fetchActivityImageList);
 
 /* 4. 留言區 */
 const reviewList = ref([]);
+
+// watch(
+//   reviewList,
+//   (newList, oldList) => {
+//     console.log("reviewList 變了！");
+//     console.log("舊值:", oldList);
+//     console.log("新值:", newList);
+//   },
+//   { deep: true } // 需要加 `deep: true` 才能監聽陣列內部變化
+// );
 
 const fetchReviewList = async () => {
   try {
@@ -404,28 +464,117 @@ const getActivityPeople = async () => {
 };
 onMounted(getActivityPeople);
 
-/* 11. 活動報名 */
-const registActivity = async () => {
-  let member = {
-    memberId: 16, // 之後這裡要改
-  };
+/* 8. 是否能留言*/
+const addReviewButton = ref("留言");
+const isAddReviewDisabled = ref(false);
 
+const getReviewIsExisied = async () => {
+  const response = await fetch(
+    `http://localhost:8080/api/activity/${props.activityId}/member/${memberId.value}/review/exist`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  let result = await response.json();
+  console.log(result);
+  if (result.action) {
+    addReviewButton.value = "已留言";
+    isAddReviewDisabled.value = true;
+  } else {
+    addReviewButton.value = "留言";
+    isAddReviewDisabled.value = false;
+  }
+};
+onMounted(getReviewIsExisied);
+
+/* 11. 活動報名 */
+const registractionStatus = ref();
+
+const getRegistractionStatus = async () => {
+  const response = await fetch(
+    `http://localhost:8080/api/activity/${props.activityId}/member/${memberId.value}/regist/status`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  let result = await response.json();
+
+  if (result.action) {
+    registractionStatus.value = "已報名";
+  } else {
+    registractionStatus.value = "報名";
+  }
+};
+onMounted(getRegistractionStatus);
+
+const registActivityConfirm = async () => {
+  const response = await fetch(
+    `http://localhost:8080/api/activity/${props.activityId}/member/${memberId.value}/regist/status`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  let regisitrationStatus = await response.json();
+
+  if (regisitrationStatus.action) {
+    const result = await Swal.fire({
+      title: "取消報名？",
+      icon: "warning",
+      allowOutsideClick: false,
+      showCancelButton: true,
+      confirmButtonText: "確認",
+      cancelButtonText: "返回",
+    });
+    if (result.isConfirmed) {
+      registActivity();
+    }
+  } else {
+    const result = await Swal.fire({
+      title: "執行報名？",
+      icon: "warning",
+      allowOutsideClick: false,
+      showCancelButton: true,
+      confirmButtonText: "確認",
+      cancelButtonText: "返回",
+    });
+    if (result.isConfirmed) {
+      registActivity();
+    }
+  }
+};
+
+const registActivity = async () => {
   const response = await fetch(
     `http://localhost:8080/api/activity/${props.activityId}/regist`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(member),
+      body: JSON.stringify(member.value),
     }
   );
   let regisitrationStatus = await response.json();
 
   if (regisitrationStatus.action) {
-    console.log("成功報名");
     currentPeople.value += 1;
+    Swal.fire({
+      title: "報名成功",
+      icon: "success",
+      confirmButtonText: "確定",
+    });
+    registractionStatus.value = "已報名";
   } else {
-    console.log("取消報名");
     currentPeople.value -= 1;
+    Swal.fire({
+      title: "報名取消",
+      icon: "error",
+      confirmButtonText: "確定",
+    });
+    registractionStatus.value = "報名";
   }
 };
 
@@ -457,6 +606,224 @@ const openRegistrationConditon = async () => {
 };
 const closeRegistrationConditon = () => {
   isPopupConditionVisible.value = false;
+};
+
+/* 13. 切換收藏 */
+const likeStatus = ref();
+
+const getLikeStatus = async () => {
+  const response = await fetch(
+    `http://localhost:8080/api/activity/${props.activityId}/member/${memberId.value}/like/status`,
+    {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+
+  let result = await response.json();
+
+  if (result.action) {
+    likeStatus.value = "已收藏";
+  } else {
+    likeStatus.value = "收藏";
+  }
+};
+onMounted(getLikeStatus);
+
+const toggleLike = async () => {
+  const response = await fetch(
+    `http://localhost:8080/api/activity/${props.activityId}/like/toggle`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(member.value),
+    }
+  );
+  let result = await response.json();
+
+  if (result.action) {
+    Swal.fire({
+      title: "成功收藏",
+      icon: "success",
+      confirmButtonText: "確定",
+    });
+    likeStatus.value = "已收藏";
+  } else {
+    Swal.fire({
+      title: "取消收藏",
+      icon: "error",
+      confirmButtonText: "確定",
+    });
+    likeStatus.value = "收藏";
+  }
+};
+
+/* 14. 新增留言 */
+const isPopupCommentVisible = ref(false);
+const commentButton = ref(false);
+const commentForm = ref({
+  memberId: 16, //  之後這裡要改
+  content: "",
+});
+
+const openComment = () => {
+  isPopupCommentVisible.value = true;
+  commentButton.value = true;
+};
+const closeComment = () => {
+  isPopupCommentVisible.value = false;
+  commentButton.value = false;
+  rewriteButton.value = false;
+};
+
+const submitComment = async () => {
+  const ask = await Swal.fire({
+    title: "確定送出？",
+    icon: "warning",
+    allowOutsideClick: false,
+    showCancelButton: true,
+    confirmButtonText: "確認",
+    cancelButtonText: "返回",
+  });
+  if (!ask.isConfirmed) {
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/activity/${props.activityId}/review/add`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(commentForm.value),
+      }
+    );
+    let result = await response.json();
+    if (result.success) {
+      Swal.fire({
+        title: "成功送出",
+        icon: "success",
+        confirmButtonText: "關閉",
+      });
+      closeComment();
+
+      addReviewButton.value = "已留言";
+      isAddReviewDisabled.value = true;
+    }
+  } catch (error) {
+    console.error("新增留言失敗:", error);
+  }
+};
+
+/* 15. 修改留言 */
+const reviewIdForRewrite = ref();
+const rewriteButton = ref(false);
+
+const openRewirte = async (reviewId) => {
+  isPopupCommentVisible.value = true;
+  rewriteButton.value = true;
+  reviewIdForRewrite.value = reviewId;
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/activity/review/${reviewId}`,
+      {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    let result = await response.json();
+    commentForm.value.content = result.review.reviewContent;
+  } catch (error) {
+    console.error("獲取留言失敗:", error);
+  }
+};
+
+const submitRewrite = async (reviewId) => {
+  const ask = await Swal.fire({
+    title: "確定送出？",
+    icon: "warning",
+    allowOutsideClick: false,
+    showCancelButton: true,
+    confirmButtonText: "確認",
+    cancelButtonText: "返回",
+  });
+  if (!ask.isConfirmed) {
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/activity/review/${reviewId}/rewrite`,
+      {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(commentForm.value),
+      }
+    );
+    let result = await response.json();
+
+    Swal.fire({
+      title: "成功送出",
+      icon: "success",
+      confirmButtonText: "關閉",
+    });
+
+    closeComment();
+
+    const updatedReview = reviewList.value.find(
+      (review) => review.reviewId === reviewId //  find()找到reviewList陣列中符合reviewId的留言
+    );
+    if (updatedReview) {
+      updatedReview.reviewTime = new Date();
+      updatedReview.reviewContent = result.review.reviewContent; // 更新留言內容
+    }
+  } catch (error) {
+    console.error("修改留言失敗:", error);
+  }
+};
+
+/* 16. 刪除留言 */
+const deleteComment = async (reviewId) => {
+  const ask = await Swal.fire({
+    title: "確定刪除？",
+    icon: "warning",
+    allowOutsideClick: false,
+    showCancelButton: true,
+    confirmButtonText: "確認",
+    cancelButtonText: "返回",
+  });
+  if (!ask.isConfirmed) {
+    return;
+  }
+
+  try {
+    const response = await fetch(
+      `http://localhost:8080/api/activity/review/${reviewId}/delete`,
+      {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+    let result = await response.json();
+
+    Swal.fire({
+      title: "成功刪除",
+      icon: "success",
+      confirmButtonText: "關閉",
+    });
+
+    closeComment();
+
+    reviewList.value = reviewList.value.filter(
+      (review) => review.reviewId !== reviewId // 過濾reviewId等於reviewId的留言
+    );
+
+    addReviewButton.value = "留言";
+    isAddReviewDisabled.value = false;
+  } catch (error) {
+    console.error("刪除留言失敗:", error);
+  }
 };
 </script>
 
@@ -495,5 +862,37 @@ const closeRegistrationConditon = () => {
   overflow-y: auto; /* 當內容超過 max-height 時顯示垂直滾動條 */
   border: 1px solid #ccc; /* 可選，增加邊框以區分區塊 */
   padding: 10px; /* 可選，增加內邊距 */
+}
+
+/* 圖片自訂按鈕樣式 */
+.custom-prev,
+.custom-next {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  background: rgba(0, 0, 0, 0.5);
+  color: white;
+  border: none;
+  padding: 25px;
+  cursor: pointer;
+  font-size: 20px;
+  z-index: 10;
+}
+
+.custom-prev {
+  left: 10px;
+}
+.custom-next {
+  right: 10px;
+}
+
+.custom-prev:hover,
+.custom-next:hover {
+  background: rgba(0, 0, 0, 0.8);
+}
+
+/* Sweet Alert */
+.swal2-container {
+  z-index: 9999 !important; /* 設定比你的自定義視窗更高 */
 }
 </style>
